12f8b8846f0abb675a9c576ec36af2b3215ad1d8
Gwen Latasa <gxlatasa@gmail.com>
2017-12-12 11:12:16 -0500

    made style changes

--- script.js
+++ script.js
@@ -36,7 +36,8 @@ function turnOffPlayerClickEvents(){
   for( x in colorsAvailable){
     $('.' + colorsAvailable[x] +'-circle').off()
   }//end for
-  //FIX  need to enable startButton
+
+  //enable startButton
   $('.start-bttn').prop('disabled', false)
 }
 
@@ -101,9 +102,7 @@ function checkIfCorrect(){
 
 
 function startGame( event ) {
-    console.log('Start game has been clicked')
-  //FIX- disable start button???
-
+  //disable start button
   $('.start-bttn').prop('disabled', true)
 
   //on click generate random color pattern
@@ -119,10 +118,10 @@ function startGame( event ) {
   //FIX-- check logic of this?
   // console.log("assuming the user won't try to play until all the colors flash...")
   //could set an interval to check if player toggle set to true???
+
   //set player toggle equal to true
   playerToggle = true
 
   turnOnPlayerClickEvents()
-
 
 }//end startGame()
